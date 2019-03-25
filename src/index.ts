028d0a7bab050a255fb994c9a2d9f2f91d1b4ed3
JustAddBri <48483935+JustAddBri@users.noreply.github.com>
2019-03-25 14:30:51 +0000

    yay validation

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,29 @@ const billInput = document.querySelector('#baseAmount') as HTMLInputElement;
 billInput.addEventListener('keyup', baseAmountChanged);
 
 let percentageTip: number;
-let billAmount: number = 0.00;
-let tipAmount: number = 10.00;
-let total: number = 0.00;
+let billAmount: number;
+let tipAmount: number;
+let total: number;
 
 
 
 function baseAmountChanged() {
     const input = this as HTMLInputElement;
-    if (input.checkValidity) {
-        updateTotals();
+    if (validateInput(input) && percentageTip != undefined) {
+        updateTotals(false);
     } else {
-        input.classList.add('.bad-input');
+        updateTotals(true);
+    }
+}
+
+
+function validateInput(input: HTMLInputElement): boolean {
+    if (input.valueAsNumber > 0) {
+        input.classList.remove('bad-input');
+        return true;
+    } else {
+        input.classList.add('bad-input');
+        return false;
     }
 }
 
@@ -37,30 +48,36 @@ function tipButtonClick() {
     });
 
     percentageTip = parseFloat(clickedBtn.value);
-    updateTotals();
-
-}
-
-function updateTotals() {
     let tipPercentDisplay = document.querySelectorAll('.tipPercent');
     tipPercentDisplay.forEach(element => {
         const display = element as HTMLSpanElement;
         display.innerText = `${percentageTip}%`;
     });
 
-    //get base amount
-    billAmount = (document.querySelector('#baseAmount') as HTMLInputElement).valueAsNumber;
+    if (validateInput(billInput)) { updateTotals(false); }
+
+}
+
+function updateTotals(resetToDefault: boolean) {
+
+    if (resetToDefault) {
+        billAmount = undefined;
+        tipAmount = undefined;
+        total = undefined;
+    }
+    else {
+        billAmount = (document.querySelector('#baseAmount') as HTMLInputElement).valueAsNumber;
+        tipAmount = (billAmount * percentageTip / 100);
+        total = billAmount + tipAmount;
+    }
+
     const billDisplay = document.querySelector('#billAmount') as HTMLSpanElement;
-    billDisplay.innerText = `\$${billAmount}`;
+    billDisplay.innerText = `\$${billAmount.toFixed(2)}`;
 
-    //tip = base x tip percent
-    tipAmount = billAmount * percentageTip / 100;
     const tipAmountDisplay = document.querySelector('#currentTipAmount') as HTMLSpanElement;
-    tipAmountDisplay.innerText = `\$${tipAmount}`;
+    tipAmountDisplay.innerText = `\$${tipAmount.toFixed(2)}`;
 
-    //add tip to base for total
-    total = billAmount + tipAmount;
     const totalAmountDisplay = document.querySelector('#total') as HTMLSpanElement;
-    totalAmountDisplay.innerText = `\$${total}`;
+    totalAmountDisplay.innerText = `\$${total.toFixed(2)}`;
 
 }
